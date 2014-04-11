1b3fa5e13efb2a059df8f176fb629c9c7ff50a7f
popeonarope <johnvepope@gmail.com>
2014-04-11 00:54:39 -0400

    continued tinkering witht he layout and design

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,13 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function() {
+
+	$('.popup').hide();
+
+	$('.winview').click(function(){
+		event.preventDefault();
+		$('.popup').fadeIn('slow');
+	});
+
+	$('.popup').click(function(){
+		$(this).fadeOut('slow');
+	});
+});
